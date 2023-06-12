921ace96aacd622c0b49c80b5ba2567323f0ff51
liying2002 <93809938+liying2002@users.noreply.github.com>
2023-06-12 15:47:37 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,9 +9,6 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/vue-demo/",
-  build: {
-    outDir: "docs"
-  },
   plugins: [
     vue(), 
     vueJsx(),
